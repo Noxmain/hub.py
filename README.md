bb7d9bae39b589e3d0da5775146cb69fc4eb5cb7
Noxmain <noah.brandt@me.com>
2022-02-17 23:05:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Python Hub
+
+`hub.py` is a python program to manage the execution of your python scripts in the console.
+
+The reason why `hub.py` was developed is because running python scripts in the console can be very tedious. To do this, you need to navigate to the directory of your script (e.g. `cd /Users/noxmain/Desktop/`) and run your script with `python3 script.py`.
+
+But running your scripts with `hub.py` is __much easier__. You only have to drag your script file once into the console and it will be saved in the recently executed scripts, enabling you to run it easily using autocompletion.
+
+<img src="screenshot.png"/>
+
+## Features
+
+- Displays your __recent files__, ordered by when you last ran them
+- __Autocompletes__ recent filenames when you type them
+- __Saves the paths__ to your scripts so you don't have to type them in
+
+## How to install
+
+Just __download__ the file `hub.py` and __save it in the root directory of your console__.
+
+If you save it in the root directory of your console, you do not have to navigate there when you want to run the hub. (You can easily find the root directory by typing `open .` into your console.)
+
+## How to use
+
+If you saved `hub.py` in the root directory of your console, you only have to enter `python3 hub.py` to __run it__.
+
+Within the hub you can select recent files using the up and down __arrow keys__ or by __entering the filename__.
+
+To run a new file (not from the recent files), simply __drag it into your console window__ and press Enter. It will then be stored in the recent files.
+
+To exit the hub, you can type `exit`, `quit` or simply press Enter (enter nothing).
+
+## Notice
+
+Please __do not use__ the escape key or special characters like `Ä` or `§`. These can cause strange behavior or crashes.
+
+## Technical features
+
+- Uses `cd` when running scripts, so even scripts with dependencies can be executed without problems
+- Displays the runtime after running a script
+- Displays an error message when an error occurs
+- Saves the recent files in the `.hub` file (in the same directory) in json format
---
+++ hub.py
@@ -0,0 +1,151 @@
+# by Noxmain
+# -*- coding: utf-8 -*-
+# Version 2.4
+from getch import getch
+from time import time
+import json
+import os
+
+format_left = lambda string, length: f"{string:<{length}}"
+format_center = lambda string, length: f"{string:^{length}}"
+format_right = lambda string, length: f"{string:>{length}}"
+move = lambda x, y: print(f"\033[{y};{x}H", end="", flush=True)
+clear = lambda: print("\033[H\033[0J", end="", flush=True)
+style = lambda string, *styles: "".join(map(lambda x: STYLES[x], styles)) + string + STYLES["default"]
+
+FIRST = True
+NOTFIRST = lambda: exec("global FIRST\nFIRST = False")
+STYLES = {
+    "red": "\033[31m",
+    "blue": "\033[34m",
+    "gray": "\033[90m",
+    "bold": "\033[1m",
+    "default": "\033[0m",
+}
+
+
+def load():
+    if os.path.isfile(".hub"):
+        f = open(".hub", "r")
+        loaded = json.load(f)
+        f.close()
+        return map(list, zip(*loaded))
+    else:
+        return list(), list()
+
+
+def save():
+    f = open(".hub", "w")
+    json.dump(list(zip(recent_paths, recent_names)), f)
+    f.close()
+
+
+def run():
+    save()
+    clear()
+    t = time()
+    os.system("cd " + recent_paths[0].replace(" ", "\\ ") + "; python3 " + recent_names[0])
+    input(style("Process finished in " + str(time() - t) + " seconds", "red", "bold"))
+    print("\n" * (height - 3))
+
+
+def print_window():
+    NOTFIRST() if FIRST else move(0, 0)
+    print("┌" + "─" * (width - 2) + "┐")
+    print("│" + style(format_center("PYTHON HUB", width - 2), "red", "bold") + "│")
+    print("├" + "─" * (width - 2) + "┤")
+    print("│" + style(format_left("Recent files", width - 2), "red", "bold") + "│")
+    for i in range(height - 8, -1, -1):
+        try:
+            print("│" + style(format_left(recent_names[i], width - 2), "red") + "│")
+        except IndexError:
+            print("│" + " " * (width - 2) + "│")
+    print("├" + "─" * (width - len(in_info) - 2) + style(in_info, "red", "bold") + "┤")
+    s = format_left(" > " + in_input[:width - 6] + STYLES["gray"] + in_suggestion[len(in_input):], width - 2) + " " * 5
+    print("│" + style(s[:width + 3], "blue") + "│")
+    print("└" + "─" * (width - 2) + "┘", end="", flush=True)
+    move(in_x, height - 1)
+
+
+if __name__ == '__main__':
+    recent_paths, recent_names = load()
+    in_input = ""
+    in_save = ""
+    in_suggestion = ""
+    in_info = ""
+    in_x = 5
+    in_y = -1
+    while True:
+        width = os.get_terminal_size().columns
+        height = os.get_terminal_size().lines
+        print_window()
+        ch = getch()
+        if ch in ["\x1b"]:
+            ch = getch() + getch()
+            if ch in ["[A"]:  # up
+                in_save = in_input if in_y == -1 else in_save
+                in_y = min(in_y + 1, len(recent_names) - 1)
+                in_input = recent_names
+            if ch in ["[B"]:  # down
+                in_y = max(in_y - 1, -1)
+                in_input = recent_names + [in_save]
+            if ch in ["[A", "[B"]:
+                in_input = in_input[in_y]
+                in_suggestion = ""
+                in_x = 5 + len(in_input)
+            if ch in ["[C"]:  # right
+                in_x = min(in_x + 1, 5 + len(in_input), width - 2)
+            if ch in ["[D"]:  # left
+                in_x = max(in_x - 1, 5)
+            in_info = ""
+        elif ch in ["\n", "\r", ""]:
+            if in_input.lower() in ["\n", "\r", " ", "", "exit", "quit"]:
+                save()
+                clear()
+                exit()
+            in_input = in_input[:-1] if in_input[-1] == " " else in_input
+            in_input = in_input.replace("\\ ", " ")
+            if (in_input in recent_names) or (in_suggestion in recent_names):
+                index = recent_names.index(in_input if in_input in recent_names else in_suggestion)
+                if os.path.isfile(os.path.join(recent_paths[index], recent_names[index])):
+                    recent_names = [recent_names.pop(index)] + recent_names
+                    recent_paths = [recent_paths.pop(index)] + recent_paths
+                    run()
+                else:
+                    in_info = " file does not exist anymore "
+            elif os.path.isfile(in_input):
+                in_input = os.path.abspath(in_input)
+                tmp = lambda x: os.path.join(x[0], x[1]) == in_input
+                tmp = list(map(tmp, zip(recent_paths, recent_names)))
+                if True in tmp:
+                    index = tmp.index(True)
+                    recent_names = [recent_names.pop(index)] + recent_names
+                    recent_paths = [recent_paths.pop(index)] + recent_paths
+                else:
+                    recent_names = [os.path.basename(in_input)] + recent_names
+                    recent_paths = [os.path.dirname(in_input)] + recent_paths
+                run()
+            else:
+                in_info = " invalid path "
+            in_input = ""
+            in_save = ""
+            in_suggestion = ""
+            in_x = 5
+            in_y = -1
+        elif ch in ["\x7f"]:
+            in_input = in_input if in_x == 5 else (in_input[:in_x - 6] + in_input[in_x - 5:])
+            in_x = max(in_x - 1, 5)
+            if len(in_input) > 0:
+                in_suggestion = list(filter(lambda x: x.startswith(in_input), recent_names))
+                in_suggestion = in_suggestion[0] if len(in_suggestion) > 0 else ""
+            else:
+                in_suggestion = ""
+        else:
+            if in_x == width - 2:
+                in_input = in_input + ch
+            else:
+                in_input = in_input[:in_x - 5] + ch + in_input[in_x - 5:]
+            in_suggestion = list(filter(lambda x: x.startswith(in_input), recent_names))
+            in_suggestion = in_suggestion[0] if len(in_suggestion) > 0 else ""
+            in_x = min(in_x + 1, 5 + len(in_input), width - 2)
+            in_info = ""
